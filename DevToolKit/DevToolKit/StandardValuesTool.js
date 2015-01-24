7708fc3f232b1bce10e8ea4a4696744e36d4f4e5
sobek1985 <sobek1985@gmail.com>
2015-01-24 09:58:20 +0000

    added page code

---
+++ DevToolKit/DevToolKit/StandardValuesTool.js
@@ -0,0 +1,22 @@
+﻿require.config({
+    paths: {
+        entityService: "/sitecore/shell/client/Services/Assets/lib/entityservice"
+    }
+});
+
+define(["sitecore", "jquery", "underscore", "entityService"], function (Sitecore, $, _, entityService) {
+    var StandardValuesTool = Sitecore.Definitions.App.extend({
+
+        initialized: function () {
+         
+        },
+
+        initialize: function () {
+         
+        },
+
+
+    });
+
+    return StandardValuesTool;
+});
