a449cd602e69cdb546c9849b3da7384910ca1a09
sobek1985 <sobek1985@gmail.com>
2015-01-24 13:26:13 +0000

    DA

---
+++ DevToolKit/DevToolKit/DataAccess.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevToolKit
+{
+    public class DataAccess
+    {
+        public Item GetItem(string id)
+        {
+            ID sId = ID.Null;
+
+            if (ID.TryParse(id, out Sid))
+            {
+                var item = Sitecore.Data.Database.GetDatabase("database").GetItem(sId);
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+        }
+    }
+}
