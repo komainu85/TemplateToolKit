68b0a2127c98dc2b6dea7b7b896ae91f016fa60b
sobek1985 <sobek1985@gmail.com>
2015-01-24 11:34:48 +0000

    missing model

---
+++ DevToolKit/DevToolKit/Models/SitecoreField.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DevToolKit.Models
+{
+    public class SitecoreField : Sitecore.Services.Core.Model.EntityIdentity
+    {
+        public string itemId { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string StandardValue { get; set; }
+    }
+}
