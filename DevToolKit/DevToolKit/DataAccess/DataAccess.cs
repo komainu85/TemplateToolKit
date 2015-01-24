11ea51501b1eca8b01af41ed6179baac9846b41c
jonathan.robbins <jonathan.m.robbins@gmail.com>
2015-01-24 14:21:22 +0000

    Arch / Moved location of DataAccess

--- DevToolKit/DevToolKit/DataAccess/DataAccess.cs
+++ DevToolKit/DevToolKit/DataAccess/DataAccess.cs
@@ -1,20 +1,19 @@
-﻿using Sitecore.Data;
+﻿using System;
+using Sitecore.Data;
 using Sitecore.Data.Items;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Sitecore.Diagnostics;
 
-namespace DevToolKit
+namespace DevToolKit.DataAccess
 {
     public class DataAccess
     {
         public Item GetItem(string id)
         {
+            Assert.IsNotNullOrEmpty(id, "Id can not be null");
+
             Item item = null;
 
             ID sId = ID.Null;
-
             if (ID.TryParse(id, out sId))
             {
                 item = Sitecore.Data.Database.GetDatabase("database").GetItem(sId);
